2274e905c1a5f40a90eb0d9ec259c07045b3c83e
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2025-04-03 10:55:34 +0100

    Add support for encrypted registry values, listing keys and improve typing

--- igelfs/registry.py
+++ igelfs/registry.py
@@ -4,7 +4,6 @@ import gzip
 import re
 from collections.abc import Iterable
 from pathlib import Path
-from typing import Any
 from xml.etree import ElementTree
 
 from igelfs.device import Cryptsetup, Mount
@@ -19,8 +18,10 @@ except ImportError:
 else:
     _KEYRING_AVAILABLE = True
 
-type ElementDict = dict[ElementTree.Element, ElementDict | str | None]
-type RecursiveDict = dict[str, RecursiveDict | Any]
+type Primitive = int | float | str | bool | None
+type RecursiveDict[K, V] = dict[K, RecursiveDict[K, V] | V]
+type ElementText = str | None
+type ElementDict = RecursiveDict[ElementTree.Element, ElementDict | ElementText]
 
 
 class XmlHelper:
@@ -33,15 +34,37 @@ class XmlHelper:
             child: (
                 children
                 if (children := cls.to_dict(child))
-                else (child.text.strip() if child.text else child.text)
+                else (text.strip() if (text := child.text) else text)
             )
             for child in element
         }
 
     @classmethod
+    def keys(
+        cls: type["XmlHelper"],
+        element: ElementTree.Element,
+        separator: str = ".",
+        prefix: list[str] | None = None,
+    ) -> list[str]:
+        """Return flat list of keys separated by separator."""
+        prefix = prefix or []
+        data = cls.to_dict(element)
+        keys = []
+        for element, value in data.items():
+            if isinstance(value, dict):
+                keys.extend(
+                    cls.keys(
+                        element, separator=separator, prefix=[*prefix, element.tag]
+                    )
+                )
+            else:
+                keys.append(separator.join([*prefix, element.tag]))
+        return keys
+
+    @classmethod
     def convert_elements_to_strings(
         cls: type["XmlHelper"], elements: ElementDict
-    ) -> RecursiveDict:
+    ) -> RecursiveDict[str, ElementText]:
         """Convert dictionary of elements to strings."""
         return {
             key.tag: (
@@ -54,8 +77,8 @@ class XmlHelper:
 
     @classmethod
     def convert_xml_types(
-        cls: type["XmlHelper"], elements: RecursiveDict
-    ) -> RecursiveDict:
+        cls: type["XmlHelper"], elements: RecursiveDict[str, ElementText]
+    ) -> RecursiveDict[str, Primitive]:
         """Convert strings in dictionary to Python types."""
         return {
             key: (
@@ -67,20 +90,22 @@ class XmlHelper:
         }
 
     @staticmethod
-    def convert_xml_type(value: str | None) -> Any:
+    def convert_xml_type(value: str | None) -> Primitive:
         """Convert string to Python type."""
-        if not value:
+        if not value:  # includes empty string
             return None
-        try:
-            return int(value)
-        except ValueError:
-            match value.lower():
-                case "true":
-                    return True
-                case "false":
-                    return False
-                case _:
-                    return value
+        for type_ in (int, float):
+            try:
+                return type_(value)
+            except ValueError:
+                pass
+        match value.lower():
+            case "true":
+                return True
+            case "false":
+                return False
+            case _:
+                return value
         raise ValueError("Unable to convert value")
 
 
@@ -89,6 +114,8 @@ class Registry:
 
     WFS_PARTITION_MINOR = 255
     GROUP_GZ_FILENAME = "group.ini.gz"
+    KEY_SEPARATOR = "."
+    DEFAULT_PASSWORD = 0x43
 
     def __init__(self, data: str) -> None:
         """Initialise registry data."""
@@ -104,22 +131,68 @@ class Registry:
         data = "\n".join(["<root>", data.strip(), "</root>"])
         return data
 
-    def get(self, key: str | Iterable[str]) -> str | None:
+    def get(
+        self, key: str | Iterable[str], separator: str = KEY_SEPARATOR
+    ) -> Primitive:
         """Get value from registry."""
         if isinstance(key, str):
-            key = key.split(".")
+            key = key.split(separator)
         parent = self.root
         for part in key:
             if (child := parent.find(part)) is None:
                 raise ValueError(f"Key '{part}' not found in registry")
             parent = child
-        return parent.text
+        return XmlHelper.convert_xml_type(parent.text)
 
-    def to_dict(self) -> RecursiveDict:
+    def get_crypt(self, key: str | Iterable[str]) -> str:
+        """Get and decrypt value from registry."""
+        value = self.get(key)
+        if not isinstance(value, str):
+            raise TypeError("Cannot decrypt non-string value")
+        return self.decrypt(value)
+
+    def keys(self, separator: str = KEY_SEPARATOR) -> list[str]:
+        """Return flat list of registry keys separated by separator."""
+        return XmlHelper.keys(self.root, separator=separator)
+
+    def to_dict(self) -> RecursiveDict[str, Primitive]:
         """Return dictionary of registry data."""
         return XmlHelper.convert_xml_types(
             XmlHelper.convert_elements_to_strings(XmlHelper.to_dict(self.root))
         )
+
+    @staticmethod
+    def encrypt(plaintext: str, password: int = DEFAULT_PASSWORD) -> str:
+        """Encrypt plaintext for registry and return result."""
+        prefix = hex(len(plaintext) + 1)[2:].zfill(4) + hex(password)[2:]
+        ciphertext = []
+        result = 0
+        for char in plaintext:
+            result = ord(char) ^ result ^ password
+            enc_char = hex(result)[2:]
+            ciphertext.append(enc_char.zfill(2))
+        return prefix + "".join(ciphertext)
+
+    @staticmethod
+    def decrypt(ciphertext: str, strict: bool = True) -> str:
+        """Decrypt ciphertext from registry and return result."""
+        length, password, data = (
+            int(ciphertext[:4], 16) - 1,
+            int(ciphertext[4:6], 16),
+            ciphertext[6:],
+        )
+        plaintext = []
+        prev_chunk = 0
+        for idx in range(0, len(data), 2):
+            chunk = int(data[idx : idx + 2], 16)
+            result = chunk ^ prev_chunk ^ password
+            plaintext.append(chr(result))
+            prev_chunk = chunk
+        if strict and length != len(plaintext):
+            raise ValueError(
+                f"Length '{length}' does not equal length of data '{len(plaintext)}'"
+            )
+        return "".join(plaintext)
 
     @classmethod
     def from_filesystem(cls: type["Registry"], filesystem: Filesystem) -> "Registry":
